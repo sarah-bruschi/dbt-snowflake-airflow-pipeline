b06e16e699807f8b5e5487263814b01b6871c932
sarah.bruschi <sarah.bruschi@gmail.com>
2025-12-26 15:59:47 -0700

    Add discounted_amount macro and integrate into int_order_items model

--- airflow_data_pipeline/models/marts/int_order_items.sql
+++ airflow_data_pipeline/models/marts/int_order_items.sql
@@ -2,9 +2,14 @@ select
 line_item.order_item_key
 ,line_item.part_key
 ,line_item.line_number
+,line_item.extended_price
 ,orders.order_key
 ,orders.customer_key
 ,orders.order_date
+,{{discounted_amount(
+    'line_item.extended_price', 
+    'line_item.discount_percentage'
+    ) }} item_discount_amount
 from 
 {{ ref('stg_tpch_orders') }} orders 
 join
